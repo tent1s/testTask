02295e460f1362bf29cad5d8ca0db0e6577434f0
roman <roman.voronov@effective.band>
2021-12-31 00:13:49 +0600

    add layout for main screen

--- app/src/main/java/com/example/testapplt/ui/screen/SearchBooksFragment.kt
+++ app/src/main/java/com/example/testapplt/ui/screen/SearchBooksFragment.kt
@@ -1,5 +1,11 @@
 package com.example.testapplt.ui.screen
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import com.example.testapplt.R
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -13,5 +19,34 @@ class SearchBooksFragment : Fragment(R.layout.fragment_search_books){
         fun newInstance() = SearchBooksFragment()
     }
 
-    private val viewBinding: FragmentSearchBooksBinding by viewBinding()
+    private val binding: FragmentSearchBooksBinding by viewBinding()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initToolbar()
+    }
+
+
+
+    private fun initToolbar(){
+        with(binding.searchBooksToolbar.searchBooksToolbarEditText) {
+            setOnEditorActionListener { _, action, _ ->
+                if (action == EditorInfo.IME_ACTION_SEARCH) {
+                    performSearch()
+                    true
+                }else
+                    false
+            }
+        }
+
+        binding.searchBooksToolbar.searchBooksToolbarFilterAppCompatImageButton.setOnClickListener {
+
+        }
+    }
+
+    private fun performSearch(){
+        TODO()
+    }
+
 }
